9993f10a85937011b30a4dd4502456e67732f841
BenjaminLM95 <benjamin.lerner95@gmail.com>
2025-03-27 13:27:30 -0300

    Creating the scripts for the game, level management. Drawing Gizmos, change scene management, game state management.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject MainMenuUI;
+    public GameObject PausedUI;
+    public GameObject OptionsUI;
+    public GameObject playerUI;
+    public GameObject ScoreUI;
+
+    public void EnableMainMenuUI()
+    {
+        DisableAllUI();
+        MainMenuUI.gameObject.SetActive(true);
+        Cursor.visible = true;
+
+    }
+
+    public void EnableGamePlay()
+    {
+        DisableAllUI();
+        playerUI.gameObject.SetActive(true);
+        ScoreUI.gameObject.SetActive(true);
+        Cursor.visible = false;
+
+    }
+
+    public void EnablePause()
+    {
+        PausedUI.gameObject.SetActive(true);
+        Cursor.visible = true;
+
+    }
+
+    public void EnableOptions()
+    {
+        DisableAllUI();
+        OptionsUI.gameObject.SetActive(true);
+    }
+
+    public void DisablePause()
+    {
+        PausedUI.gameObject.SetActive(false);
+
+    }
+
+    public void DisableAllUI()
+    {
+        PausedUI.gameObject.SetActive(false);
+        MainMenuUI.gameObject.SetActive(false);
+        OptionsUI.gameObject.SetActive(false);
+        playerUI.gameObject.SetActive(false);
+        ScoreUI.gameObject.SetActive(false);
+    }
+}
